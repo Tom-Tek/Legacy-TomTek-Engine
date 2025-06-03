c449b09ed6357240175281953a311ef552b72bc3
Liam <rousselle.mail@gmail.com>
2025-06-02 20:23:25 -0600

    Solution Setup

---
+++ EngineHub/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using EngineHub.Source;
+using System.Windows;
+
+namespace EngineHub
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void HelloWorldClicked(object sender, RoutedEventArgs e)
+        {
+            MyBlock.Text = sender.ToString();
+            MyButton.Content = e.ToString();
+
+            ProjectMaker.HelloWorld();
+        }
+    }
+}
---
+++ EngineHub/Source/ProjectMaker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineHub.Source
+{
+    class ProjectMaker
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
